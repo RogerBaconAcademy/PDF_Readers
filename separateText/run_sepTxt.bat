55be054f9c8b7a525ea5c2b25c5d946f6e2bd60b
jeremiah-carlson <87202597+jeremiah-carlson@users.noreply.github.com>
2022-03-02 11:59:43 -0500

    Delete separateText directory

--- separateText/run_sepTxt.bat
+++
@@ -1,2 +0,0 @@
-cd /D "%~dp0" && ^
-if exist sepTxt_env\ (sepTxt_env\Scripts\activate.bat && python separateText.py) else (python -m venv sepTxt_env && pip install -r requirements.txt && sepTxt_env\Scripts\activate.bat && timeout 4 && python separateText.py)
